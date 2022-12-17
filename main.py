c1ed666e51ee47d41cbb957671c750721865c9db
Jeremy <bilyjeremy@gmail.com>
2022-12-17 23:25:03 +0100

    Ajout définition duree execution

---
+++ main.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# coding utf-8
+
+import time
+
+def display_name():
+    """Affiche la durée d'execution du programme BlueCrack"""
+    print("Durée : " + str(time.time() - debut) + " secondes")
+    
+debut = time.time()
